7fa6807873fedf29e6c0ee3e477c0ca534f955f8
Bibhas Bhattacharya <bibhas2@gmail.com>
2018-07-09 22:09:15 -0400

    Add modal dialog

--- projects/popper-lib/src/lib/popper-lib.module.ts
+++ projects/popper-lib/src/lib/popper-lib.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MenuComponent } from './menu/menu.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [MenuComponent],
+  declarations: [MenuComponent, ModalComponent],
   exports: [MenuComponent]
 })
 export class PopperLibModule { }
